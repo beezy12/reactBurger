eb8087d9b746b2d9a3d823662d52f6cc44e7d19e
Brian Stumbaugh <brianstumbaugh@brians-mbp.attlocal.net>
2018-01-18 20:58:07 -0600

    adding burger ingredients dynamically using map

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,20 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    // state is being passed in as an object, so need to convert to an array to use .map
+    // the .keys method turns the object keys into an array of keys as strings, the values are not part of the array
+    console.log(Object.keys(props.ingredients))
+
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            });
+        });
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top' />
-            <BurgerIngredient type='cheese' />
-            <BurgerIngredient type='meat' />
+            {transformedIngredients}
             <BurgerIngredient type='bread-bottom' />
         </div>
     );
