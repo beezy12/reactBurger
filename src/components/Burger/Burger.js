fefa66cbdfe7244b1081276c7658c002092df1f8
Brian Stumbaugh <brianstumbaugh@brians-mbp.attlocal.net>
2018-01-18 21:08:44 -0600

    handling no ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,14 +6,20 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) => {
     // state is being passed in as an object, so need to convert to an array to use .map
     // the .keys method turns the object keys into an array of keys as strings, the values are not part of the array
+    // we then use reduce to flatten the array (?), in case there are no items to start with (?)
     console.log(Object.keys(props.ingredients))
-
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />;
             });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>please start adding ingredients</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top' />
